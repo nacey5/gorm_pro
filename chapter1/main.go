7128881fe466ed155bad15a36ced429aaf070749
nacey5 <1160124552@qq.com>
2023-01-06 18:08:21 +0800

    工具类~~

---
+++ chapter1/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+)
+
+var name string
+
+type Name string
+
+func (i *Name) String() string {
+	return fmt.Sprint(*i)
+}
+
+func (i *Name) Set(value string) error {
+	if len(*i) > 0 {
+		return errors.New("name flag already set")
+	}
+
+	*i = Name("eddycjy:" + value)
+	return nil
+}
+
+func main() {
+	var sm Name
+	flag.Var(&sm, "name", "帮助信息")
+	flag.Parse()
+
+	log.Printf("name: %s", sm)
+}
+
+func t2() {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "Go 语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "n", "PHP 语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	log.Printf("name: %s", name)
+}
+
+func t1() {
+	var name string
+	flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
+	flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
+	flag.Parse()
+
+	log.Printf("name: %s", name)
+}
